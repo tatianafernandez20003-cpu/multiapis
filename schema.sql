582d297085e52230369e138915c91a80918855b5
tatianafernandez20003-cpu <tatianafernandez20003@gmail.com>
2025-10-01 19:32:21 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,23 +1,11 @@
--- users
-CREATE SCHEMA IF NOT EXISTS users_schema AUTHORIZATION ndmcadmin;
-
-CREATE TABLE IF NOT EXISTS users_schema.users (
+-- 1) Crear un esquema específico para Users
+    CREATE SCHEMA IF NOT EXISTS users_schema AUTHORIZATION btfpadmin;
+    -- 2) Crear la tabla dentro del esquema
+    CREATE TABLE IF NOT EXISTS users_schema.users (
     id SERIAL PRIMARY KEY,
     name  TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL
-);
-
-GRANT ALL PRIVILEGES ON SCHEMA users_schema TO ndmcadmin;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA users_schema TO ndmcadmin;
-
--- products
-CREATE SCHEMA IF NOT EXISTS products_schema AUTHORIZATION ndmcadmin;
-
-CREATE TABLE IF NOT EXISTS products_schema.products (
-  id    SERIAL PRIMARY KEY,
-  name  TEXT NOT NULL,
-  price NUMERIC(10,2) NOT NULL
-);
-
-GRANT ALL PRIVILEGES ON SCHEMA products_schema TO ndmcadmin;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA products_schema TO ndmcadmin;
+    );
+    -- 3) (Opcional) Dar privilegios al usuario administrador
+    GRANT ALL PRIVILEGES ON SCHEMA users_schema TO btfpadmin;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA users_schema TO btfpadmin;
